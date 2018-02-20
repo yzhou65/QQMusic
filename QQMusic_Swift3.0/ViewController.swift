87cccd461bc540bb5cfdc974a046f066a9f5d3a2
Yue <yue@yues-macbook-pro.local>
2018-02-20 06:02:47 -0600

    Initial Commit

---
+++ QQMusic_Swift3.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QQMusic_Swift3.0
+//
+//  Created by Yue Zhou on 2/20/18.
+//  Copyright © 2018 Yue Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
